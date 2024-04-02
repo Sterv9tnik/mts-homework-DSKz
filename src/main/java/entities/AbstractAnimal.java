fce310505487f278b51e8938f8dccfc64c646bd0
Sterv9tnik <akamash@yandex.ru>
2024-04-02 21:16:15 +0300

    override equals and hashcode, add getAge

--- src/main/java/entities/AbstractAnimal.java
+++ src/main/java/entities/AbstractAnimal.java
@@ -6,6 +6,7 @@ import lombok.ToString;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -19,6 +20,19 @@ public abstract class AbstractAnimal implements Animal {
         this.cost = cost;
         this.character = character;
         this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractAnimal that = (AbstractAnimal) o;
+        return Objects.equals(breed, that.breed) && Objects.equals(name, that.name) && Objects.equals(cost, that.cost) && Objects.equals(character, that.character) && Objects.equals(birthDate, that.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(breed, name, cost, character, birthDate);
     }
 
     protected String breed;
@@ -77,5 +91,20 @@ public abstract class AbstractAnimal implements Animal {
         }
 
         return currentDate;
+    }
+
+    @Override
+    public int getAge() {
+        int yearDifferent = LocalDate.now().getYear() - getBirthDate().getYear();
+        int monthDifferent = LocalDate.now().getMonthValue() - getBirthDate().getMonthValue();
+        int dayDifferent = LocalDate.now().getDayOfMonth() - getBirthDate().getDayOfMonth();
+
+        if (yearDifferent != 0 && monthDifferent < 0) {
+            yearDifferent--;
+        } else if (yearDifferent != 0 && monthDifferent == 0 && dayDifferent < 0) {
+            yearDifferent--;
+        }
+
+        return yearDifferent;
     }
 }
