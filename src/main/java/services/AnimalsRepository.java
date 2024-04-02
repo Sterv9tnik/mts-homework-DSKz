db952e577e8562aa9d5f14f9d8f5d8717cdc1bc9
Sterv9tnik <akamash@yandex.ru>
2024-04-02 21:16:35 +0300

    create AnimalsRepository

---
+++ src/main/java/services/AnimalsRepository.java
@@ -0,0 +1,16 @@
+package services;
+
+import entities.Animal;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public interface AnimalsRepository {
+
+    Map<String, LocalDate> findLeapYearNames(List<Animal> animalList);
+
+    Map<Animal, Integer> findOlderAnimal(List<Animal> animalList, int age);
+
+    Map<String, Integer> findDuplicate(List<Animal> animalList);
+}
